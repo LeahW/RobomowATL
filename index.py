27be8464839dd794ed28281956f2611888ef7fc6
Leah Williamson <leahelizabethwilliamson@gmail.com>
2014-10-27 21:49:27 -0400

    Update index.py
    omit final print of close tags (troubleshooting)

--- index.py
+++ index.py
@@ -5,5 +5,3 @@ import random
 slogan = ["It mows. You don't.", "It mows. You get your time back.", "Just a humble robot lawnmower. Mowing turf in the ATL."]
 
 print("<html><header><title>RobomowATL</title></header><body>"), print (random.choice(slogan)), print ("</body><p hidden>")
-
-print("</p>,</html>")
