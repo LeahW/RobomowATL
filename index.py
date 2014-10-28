d3529adc31d83cdc25e805ed89516ba3731f10be
Leah Williamson <leahelizabethwilliamson@gmail.com>
2014-10-27 21:48:12 -0400

    Update index.py
    omit final request for input

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+
 import random
+
 slogan = ["It mows. You don't.", "It mows. You get your time back.", "Just a humble robot lawnmower. Mowing turf in the ATL."]
+
 print("<html><header><title>RobomowATL</title></header><body>"), print (random.choice(slogan)), print ("</body><p hidden>")
 
 print("</p>,</html>")
-input()
