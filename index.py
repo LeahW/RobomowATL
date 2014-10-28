24efbab18a84e8e1046a3798a02c2c25a5d330f1
Leah Williamson <leahelizabethwilliamson@gmail.com>
2014-10-27 21:59:10 -0400

    No Py for you. Restore last HTML

--- index.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import random
-
-slogan = ["It mows. You don't.", "It mows. You get your time back.", "Just a humble robot lawnmower. Mowing turf in the ATL."]
-
-print("<html><header><title>RobomowATL</title></header><body>"), print (random.choice(slogan)), print ("</body><p hidden>")
